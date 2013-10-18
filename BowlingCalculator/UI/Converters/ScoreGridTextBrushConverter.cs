b5919ca7bf3f709096d00316417bf1907f8ab335
Kamran Ayub <kamran.ayub@gmail.com>
2013-10-17 19:18:21 -0700

    Improve game screen styles

---
+++ BowlingCalculator/UI/Converters/ScoreGridTextBrushConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BowlingCalculator.UI.Converters {
+    public class ScoreGridTextBrushConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+
+            var score = value as string;
+
+            if (score == "/" || score == "X") {
+                return Application.Current.Resources["PhoneForegroundBrush"];
+            } else {
+                return Application.Current.Resources["PhoneSubtleBrush"];
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
